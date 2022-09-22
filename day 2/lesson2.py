edd132d33f0ee1e234bd7e4fcedb2255c11b72b5
nika1keshelava <nika.keshelava.1@btu.edu.ge>
2022-09-22 20:03:22 +0400

    changes have been made

--- day 2/lesson2.py
+++ day 2/lesson2.py
@@ -3,7 +3,8 @@ age = 20
 height = 176.5
 
 
-print("chemi saxelia {} da chemi asakia {}".format(name, age))
+print("chemi saxelia {} da chemi asakia {}".format(name, age)) 
+#formatის მნიშვნელობები ჩაჯდება ფიგურულ ფრჩხილებში {}
 
 
 print("day 3 in lesson2")
