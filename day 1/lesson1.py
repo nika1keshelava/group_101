13bd328133689ccdf56cbb545406277499af9308
nika1keshelava <nika.keshelava.1@btu.edu.ge>
2022-09-22 19:35:41 +0400

    day 1 and day 2 commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,24 @@
+name = "nika"  # string tipis cvladi
+age = 22  # int tipis cvladi     (integer  - mteli ricxvi)
+height = 184.3  # float tipis cvladi (float - atwilads)
+# bool  tipis cvladi  (boolean - logikuri cvladi, ormagi mnishvnelobis)
+knows_programming = True
+is_chad = False  # bool
+surname = "keshelava"
+
+# name aris cvladi
+# = cvladis inicializacia
+# "nika"  aris cvladis mnishvneloba
+
+
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(knows_programming))
+# print(type(is_chad))
+
+
+print(name + " " + surname)
+print(name + " " + str(age))
+
+#me mqvia nika gvaria keshelava var 22 wlis 
---
+++ day 2/lesson2.py
@@ -0,0 +1,7 @@
+name = "giorgi."
+age = 20
+height = 176.5
+
+
+print("chemi saxelia {} da chemi asakia {}".format(name, age))
+
